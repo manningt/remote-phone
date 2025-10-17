79316320894662735b2104fdbeabb22f96c9ddd0
manningt <tom2manningetal.com>
2025-10-17 10:17:33 -0400

    working using shell scripts as commands

--- set_qpwmc_mode.py
+++ set_qpwmc_mode.py
@@ -9,40 +9,61 @@ import logging
 LOG_FORMAT = ('[%(asctime)s] L%(lineno)04d %(levelname)-3s: %(message)s')
 logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
 
+def log_subprocess_output(pipe):
+   start_time = time.time()
+   for line in iter(pipe.readline, b''): # b'\n'-separated lines
+      logging.debug(f'subprocess output: {line.decode().strip()}')
+      if time.time() - start_time > 10:
+         return
+      # if 'RSSI (LTE):' in line.decode():
+      #    logging.info(f'got RSSI (LTE) line from subprocess: {line.decode().strip()}')
+      #    return
 
 def set_qpcmv():
    logging.debug('before stopping MM service')
-   mm_stop_cmd = ['sudo', 'systemctl', 'stop', 'ModemManager.service']
-   process = subprocess.Popen(mm_stop_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+   cmd = ['sudo', 'systemctl', 'stop', 'ModemManager.service']
+   process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
    output, error = process.communicate()
    if process.returncode != 0:
       logging.error(f'Failed to stop ModemManager: output={output.decode()}  error={error.decode()}')
-      return
+      sys.exit(1)
    else:
       logging.debug('Stopped ModemManager service')
    # print(f'stop MM: q output={output.decode()}  error={error.decode()}')
 
-   logging.debug('before starting MM in debug mode')
    mm_debug_cmd = ['sudo', 'ModemManager', '--debug']
-   debug_process = subprocess.Popen(mm_debug_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+   debug_process = subprocess.Popen(mm_debug_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
    logging.debug(f'Started ModemManager in debug mode; PID={debug_process.pid}')
+   with debug_process.stdout:
+      log_subprocess_output(debug_process.stdout)
    # this process will run until we kill 
-   
-   print('waiting for ModemManager to start', end='')
-   for _ in range(10):
-      print(' .', end='', flush=True)
-      time.sleep(1)
-   print(' .')
-   
-   logging.debug('before setting QPCMV')
-   set_qpcmv_cmd = ['sudo', 'mmcli', '-m', '0', "--command='+QPCMV=1,2'"]
-   process = subprocess.Popen(set_qpcmv_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+   # sending the command in the following format gets an error, so using a shell script instead
+   # cmd = ['sudo', 'mmcli', '-m', '0', '--command=\'+QPCMV=1,2\'']
+   cmd = ['/home/judy/repos/remote-phone/set_qpwmc_mode.sh']
+   process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
    output, error = process.communicate()
    if process.returncode != 0:
-      print(f'Failed to set QPCMV: output={output.decode()}  error={error.decode()}')
-      return
+      logging.fatal(f'Failed to set QPCMV: output={output.decode().rstrip()}  error={error.decode().rstrip()}')
+   else:
+      # verify the setting
+      cmd = ['/home/judy/repos/remote-phone/get_qpwmc_mode.sh']
+      process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+      output, error = process.communicate()
+      if '+QPCMV: 1,2' in output.decode():
+         logging.info('QPCMV successfully set to 1,2')
+      logging.debug(f'get QPCMV: output={output.decode().rstrip()}  error={error.decode()}')
 
-   debug_process.kill()
+   debug_process.terminate()
+   debug_process.wait()
+
+   cmd = ['sudo', 'systemctl', 'start', 'ModemManager.service']
+   process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+   output, error = process.communicate()
+   if process.returncode != 0:
+      logging.error(f'Failed to start ModemManager: output={output.decode().rstrip()}  error={error.decode()}')
+   else:
+      logging.debug('Started ModemManager service')
 
 
 if __name__ == "__main__":
